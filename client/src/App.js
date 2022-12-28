fa8723ba1edc8eddb24e5c29bc5e7165c04c0c7e
raihanputro <raihanputromaulana477@gmail.com>
2022-12-28 18:16:09 +0700

    Adding style and containers component to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <>TEST</>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 };
