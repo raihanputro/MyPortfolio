6ba32da6261a5bb111f22a1b2695004e32826090
Raihan Putro Maulana Rizky <87045526+raihanputro@users.noreply.github.com>
2022-12-28 18:24:54 +0700

    Merge main to server (#6)
    * Adding style and containers component to app index js (#5)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss'
+
 function App() {
   return (
     <div className="App">
-      <>TEST</>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
